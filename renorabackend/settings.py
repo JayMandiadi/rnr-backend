ac24ad1d0149a4f9e6e59847792ae9c412962fd3
JJ Mandiadi <jonesm@codev.com>
2023-02-19 06:10:20 +0800

    fix: set debug to false on prod

--- renorabackend/settings.py
+++ renorabackend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-il)=o_83-%6xyhl)l_dh^_mbndam$b&2(^-zyfucr3n=q15u_u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
